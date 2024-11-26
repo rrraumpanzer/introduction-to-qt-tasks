1de5715dfc7e2f0c523633d4c2ee3e982311a922
rrraumpanzer <ayylmao3713@gmail.com>
2024-11-26 19:16:37 +0300

    Second task is done.

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,6 +1,8 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -8,7 +10,28 @@ int main(int argc, char *argv[]) {
 
     window.resize(320, 240);       // Задайте размер окна
     window.setWindowTitle("Простое окно");
-    window.show();                 // Отобразите окно
+
+    QLabel *label = new QLabel("Нажмите кнопку", &window);
+    QPushButton *button1 = new QPushButton("Кнопка 1", &window);
+    QPushButton *button2 = new QPushButton("Кнопка 2", &window);
+    QObject::connect(button1, &QPushButton::clicked, [&]() 
+    {
+        label->setText("Кнопка 1 нажата");
+    });
+
+    QObject::connect(button2, &QPushButton::clicked, [&]() 
+    {
+        label->setText("Кнопка 2 нажата");
+    });
+
+
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(label);
+    layout->addWidget(button1);
+    layout->addWidget(button2);
+    window.setLayout(layout);
+    window.show();                 
 
     return app.exec();
 }
